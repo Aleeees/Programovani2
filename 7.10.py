b8d3460f5c6bc786afbfa5a3a334c245feb0cb72
Aleš Strnad <strnadales2@gmail.com>
2025-02-25 13:19:27 +0100

    Stupid alien

---
+++ 7.10.py
@@ -0,0 +1,5 @@
+#Napiš program mimozemstan.py, který pomocí barevných obdélníků nakreslí hlavu
+#mimozemšťana. Na hlavě by měly být minimálně dvě stejné oči a jedna ústa. Souřadnice
+#středu hlavy jsou uloženy v proměnných x, y.
+#Bude program fungovat správně i v případě, že hodnotu proměnné x zvětšíš o 30
+#a hodnotu proměnné y zvětšíš o 40? Jestli ne, program oprav.
